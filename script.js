7718891a9619dd96498dfdd34ecf35b9b097786b
Meir, Y <meir.y.aws@gmail.com>
2026-02-02 20:48:27 +0200

    Expand barcode scanner support (PDF417, Matrix, etc)

--- script.js
+++ script.js
@@ -159,11 +159,18 @@ function startScanner() {
                 Html5QrcodeSupportedFormats.QR_CODE,
                 Html5QrcodeSupportedFormats.CODE_128,
                 Html5QrcodeSupportedFormats.CODE_39,
+                Html5QrcodeSupportedFormats.CODE_93,
                 Html5QrcodeSupportedFormats.EAN_13,
                 Html5QrcodeSupportedFormats.EAN_8,
                 Html5QrcodeSupportedFormats.UPC_A,
                 Html5QrcodeSupportedFormats.UPC_E,
-                Html5QrcodeSupportedFormats.CODABAR
+                Html5QrcodeSupportedFormats.CODABAR,
+                Html5QrcodeSupportedFormats.PDF_417,
+                Html5QrcodeSupportedFormats.DATA_MATRIX,
+                Html5QrcodeSupportedFormats.AZTEC,
+                Html5QrcodeSupportedFormats.ITF,
+                Html5QrcodeSupportedFormats.RSS_14,
+                Html5QrcodeSupportedFormats.RSS_EXPANDED
             ],
             verbose: false
         });
